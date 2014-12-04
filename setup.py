2a257fb27a345667471f34578c4c316e6c3c9d24
Daniel Widerin <daniel@widerin.net>
2014-12-04 15:38:44 +0100

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 def read(*paths):
     return open(os.path.join(os.path.dirname(__file__), *paths), 'r').read()
 
-version = '2.0.0'
+version = '2.0.1.dev0'
 
 setup(
     name='collective.pdfpeek',
